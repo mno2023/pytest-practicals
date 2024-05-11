bb0fd6032e0db9b1fc2f3ea4cbb201ba2ee6e8d4
omkar-mn <mnomkar2023@gmail.com>
2024-05-11 18:45:46 +0530

    Adds two digits

---
+++ add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    result = a + b
+    print(result)
+
+def main():
+    print("Adding your given numbers :")
+    print("Result of adding 3 and 6: ")
+    add(3, 6)
+
+if __name__ == "__main__":
+    main()
